b3da34e95449937bb715dd8244712c5147bc81bf
Dilum De Silva <dilumdesilva@Dilums-MacBook-Pro.local>
2017-08-19 00:44:17 +0530

    double click for fullscren mode function added

--- MusicMasteByDilumDeSilva/src/musicmastebydilumdesilva/MusicMasteByDilumDeSilva.java
+++ MusicMasteByDilumDeSilva/src/musicmastebydilumdesilva/MusicMasteByDilumDeSilva.java
@@ -6,9 +6,11 @@
 package musicmastebydilumdesilva;
 
 import javafx.application.Application;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 
 /**
@@ -22,7 +24,19 @@ public class MusicMasteByDilumDeSilva extends Application {
         Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
         
         Scene scene = new Scene(root);
+        //Title of The Application
+        stage.setTitle("MusicMaster");
         
+        //A new event fired with the mouse click
+        //If user double clicked on the screen full screen mode will fired
+        scene.setOnMouseClicked(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent doubleClicked) {
+                if(doubleClicked.getClickCount() == 2){
+                    stage.setFullScreen(true);
+                }
+            }
+        });
         stage.setScene(scene);
         stage.show();
     }
